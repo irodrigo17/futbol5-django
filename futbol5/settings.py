54c307cb51a72d125786e5589d783a10461655a3
Ignacio Rodrigo <irodrigo17@gmail.com>
2015-02-11 03:40:57 -0200

    Add jd-database-url

--- futbol5/settings.py
+++ futbol5/settings.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -63,6 +66,8 @@ DATABASES = {
         'NAME': 'futbol5_django',
     }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
