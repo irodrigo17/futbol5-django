939ce9ffaabceca89f7ca4ab59ba5689c5c76144
Ignacio Rodrigo <irodrigo17@gmail.com>
2015-07-30 09:30:37 -0300

    Remove URL namespaces and tidy up

--- futbol5/urls.py
+++ futbol5/urls.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 
 
 urlpatterns = patterns('',
-    url(r'^', include('core.urls', namespace='core')),
+    url(r'^', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
 )
